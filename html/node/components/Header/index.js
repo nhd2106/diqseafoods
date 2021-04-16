eba70810232c4a4db96485c0867602b1337f569c
Duoc Ngo <nhd.ars@gmail.com>
2021-04-16 15:32:05 +0700

    refactor

--- html/node/components/Header/index.js
+++ html/node/components/Header/index.js
@@ -1,27 +1,26 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import styles from '../Styles.module.scss';
 
 export default function Index(props) {
-    const nav = document.querySelector(".nav-links");
-    const burger = document.querySelector(".burger");
-    const links = nav.querySelectorAll("a");
+    const navRef = useRef();
+    const [navOpen, setNavOpen] = useState('');
+    const [burger, setBugger] = useState('');
     const handleClick = () => {
-        nav.classList.toggle("nav-open");
-        burger.classList.toggle("toggle");
-
+        setNavOpen((prev) => !prev ? 'nav-open': '');
+        setBugger((prev) => !prev ? 'toggle': '');
     }
 
     return (
         <>
         <header className={`main-head shadow relative z-10 ${styles.mainHead}`}>
-        <nav className="flex items-center w-11/12 m-auto">
+        <nav ref={navRef} className="flex items-center w-11/12 m-auto">
             <h1 id="logo" className="flex-1">DIQ Seafoods</h1>
-            <ul className="nav-links flex items-center justify-around flex-1">
+            <ul className={`nav-links flex items-center justify-around flex-1 ${navOpen}`}>
             <li><a href="#about" onClick={handleClick}>About</a></li>
             <li><a href="#work" onClick={handleClick}>Work</a></li>
             <li><a href="#contact" onClick={handleClick}>Contact</a></li>
             </ul>
-            <div className="burger" onClick={handleClick}>
+            <div className={`burger ${burger}`} onClick={handleClick}>
                 <span className="line1"></span>
                 <span className="line2"></span>
                 <span className="line3"></span>
