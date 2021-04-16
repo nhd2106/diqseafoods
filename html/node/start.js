458d8832f2151a7e9ccccbb3e5173d5ad8454727
Duoc Ngo <nhd.ars@gmail.com>
2021-04-16 15:53:16 +0700

    port

--- html/node/start.js
+++ html/node/start.js
@@ -5,14 +5,15 @@ const next = require('next')
 const dev = process.env.NODE_ENV !== 'production'
 const app = next({ dev })
 const handle = app.getRequestHandler()
+const port = 80;
 
 app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true)
     const { pathname, query } = parsedUrl
     handle(req, res, parsedUrl)
-  }).listen(80, (err) => {
+  }).listen(port, (err) => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`> Ready on http://localhost:${port}`)
   })
 })
